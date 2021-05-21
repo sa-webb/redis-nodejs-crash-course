29b1e102d848dcdc329f32473717e646e678641b
Austin Webb <sawebb30@gmail.com>
2021-05-21 01:40:37 -0400

    feat: latestCheckin

--- src/routes/checkin_routes.js
+++ src/routes/checkin_routes.js
@@ -68,7 +68,7 @@ router.get(
     // (most recent) checkin from the stream whose key is
     // stored in checkinStreamKey.
     // https://redis.io/commands/xrevrange
-    const latestCheckin = await redisClient.xrevrange(checkinStreamKey, 'TODO');
+    const latestCheckin = await redisClient.xrevrange(checkinStreamKey, '+', '-', 'COUNT', '1');
 
     // Convert array of arrays response to array of objects.
     const response = buildCheckinObjects(latestCheckin);
